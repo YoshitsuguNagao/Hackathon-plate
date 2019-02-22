12c853ebdddf8586e35a72c070c71001251809af
franlol <fraaaaan@gmail.com>
2019-02-22 16:37:38 +0100

    First beta

--- assets/src/main.js
+++ assets/src/main.js
@@ -4,6 +4,8 @@ function main () {
   console.log('JS Running');
 
   var navbar = document.getElementById("navbar");
+  var navbarItems = document.querySelectorAll("section-1-li");
+  // var navbarHidden = document.getElementById("navbarHidden");
 
   var windowYOffset;
   window.addEventListener("scroll", function() {
@@ -11,10 +13,18 @@ function main () {
     if (windowYOffset >= navbar.offsetTop) {
       navbar.classList.add("navbar-sticky");
       navbar.classList.remove("navbar-block");
+      navbarItems.forEach(function(li) {
+        li.classList.add("section-1-li");
+      });
+      // navbarHidden.style.display = "none";
     }
     if (windowYOffset === navbar.offsetTop) {
       navbar.classList.remove("navbar-sticky");
-      navbar.classList.add("navbar-block")
+      navbar.classList.add("navbar-block");
+      navbarItems.forEach(function(li) {
+        li.classList.remove("section-1-li");
+      });
+      // navbarHidden.style.display = "block";
     }
   })
 
