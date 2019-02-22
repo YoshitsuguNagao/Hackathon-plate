098266160da1c886ffd3a900155339eac78ecff0
franlol <fraaaaan@gmail.com>
2019-02-22 14:47:07 +0100

    First grid, starting sections

--- main.js
+++ main.js
@@ -2,6 +2,24 @@
 
 function main () {
   console.log('JS Running');
+
+  var navbar = document.getElementById("navbar");
+
+  var windowYOffset;
+  window.addEventListener("scroll", function() {
+    windowYOffset = window.pageYOffset;
+    if (windowYOffset >= navbar.offsetTop) {
+      navbar.classList.add("navbar-sticky");
+      navbar.classList.remove("navbar-block");
+    }
+    if (windowYOffset === navbar.offsetTop) {
+      navbar.classList.remove("navbar-sticky");
+      navbar.classList.add("navbar-block")
+    }
+  })
+
+
+
 }
 
 window.addEventListener('load', main);
